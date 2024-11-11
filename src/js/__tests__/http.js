5c2c61a345e4c83ec3d53bf4dd9b4033abd23a23
Natali LaGuta <n.v.laguta2023@gmail.com>
2024-11-11 16:30:31 +0200

    эксперименты далее 2

--- src/js/__tests__/http.js
+++ src/js/__tests__/http.js
@@ -1,9 +1,8 @@
+/* eslint-disable no-undef */
 import fetchData from '../http';
 
-describe('Тестирование функции fetchData', () => {
-    test('должна выбросить ошибку при вызове с любым URL', () => {
-        expect(() => fetchData('https://example.com')).toThrow('Mock this!');
-        expect(() => fetchData('https://another-url.com')).toThrow('Mock this!');
-        expect(() => fetchData()).toThrow('Mock this!');
-    });
+test('должна выбросить ошибку при вызове с любым URL', () => {
+    expect(() => fetchData('https://example.com')).toThrow('Mock this!');
+    expect(() => fetchData('https://another-url.com')).toThrow('Mock this!');
+    expect(() => fetchData()).toThrow('Mock this!');
 });
