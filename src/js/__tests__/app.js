de6dce1788f789f98d01f1bcccf7919e44c169d7
Natali LaGuta <n.v.laguta2023@gmail.com>
2024-11-11 12:45:42 +0200

    Added task_2

--- src/js/__tests__/app.js
+++ src/js/__tests__/app.js
@@ -1,10 +1,53 @@
+/* eslint-disable no-undef */
+// задача 1
 import { getHealthStatus } from '../app';
 
 test.each([
     [{ name: 'X', health: 90 }, 'healthy'],
     [{ name: 'Y', health: 30 }, 'wounded'],
     [{ name: 'Z', health: 10 }, 'critical'],
-])
-('Testing function getHealthStatus with %o ожидается статус %s', (character, expected) => {
-    expect(getHealthStatus(character)).toBe(expected);
+])('Testing function getHealthStatus with %o ожидается статус %s', (character, expected) => {
+    expected(getHealthStatus(character)).toBe(expected);
+});
+
+// задача 2 Matchers
+import { sortHeroesByHealth } from '../app';
+
+describe('Тестирование сортировки героев по здоровью', () => {
+    test.each([
+        [
+            [
+                { name: 'мечник', health: 10 },
+                { name: 'маг', health: 100 },
+                { name: 'лучник', health: 80 },
+            ],
+            [
+                { name: 'маг', health: 100 },
+                { name: 'лучник', health: 80 },
+                { name: 'мечник', health: 10 },
+            ]
+        ],
+        [
+            [
+                { name: 'волшебник', health: 40 },
+                { name: 'боец', health: 90 },
+            ],
+            [
+                { name: 'боец', health: 90 },
+                { name: 'волшебник', health: 40 },
+            ]
+        ],
+        [
+            [
+                { name: 'солдат', health: 30 },
+                { name: 'разведчик', health: 30 },
+            ],
+            [
+                { name: 'солдат', health: 30 },
+                { name: 'разведчик', health: 30 },
+            ]
+        ],
+    ])('для героев %o ожидается сортировка %o', (heroes, expected) => {
+        expected(sortHeroesByHealth(heroes)).toEqual(expected);
+    });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 // TODO: write your code here
+
+// задача 1
 export function getHealthStatus(character) {
     const { health } = character;
     if (health > 50) {
@@ -8,4 +10,14 @@ export function getHealthStatus(character) {
     } else {
         return 'critical';
     }
+}
+
+// задача 2 Matchers
+// slice() - создание копии массива, чтобы не изменять исходник
+// Использовать toBe в этом случае нельзя, так как массивы не будут равны
+// по ссылке (даже если их содержимое одинаковое),
+// поэтому применяется toEqual
+
+export function sortHeroesByHealth(heroes) {
+    return heroes.slice().sort((a, b) => b.health - a.health);
 }
